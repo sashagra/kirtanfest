8cb7827b1f6fdd29ddc02e62fae9242fdafabc74
Aliaksandr <archon.san@Gmail.com>
2021-06-03 19:00:06 +0300

    start tempate

---
+++ js/custom.js
@@ -0,0 +1,80 @@
+// JavaScript Document
+// Author Name: Saptarang
+// Author URI: http://www.saptarang.org
+// Themeforest: http://themeforest.net/user/saptarang?ref=saptarang
+// Creation Date: 8 April, 2015
+
+/* CUSTOM PARAMETERS */
+			
+/* ================================  CONTACT PAGE  =================================== */
+			
+	/* SET CONTACT FORM SUCCESS MESSAGE */
+	
+			var submitMessage = "Your form has been submitted successfully.";
+			
+			var successParagraph = "We'll respond to your message within 24 hours. <br> Please call <b>123.456.789</b> for any urgent queries. Thank You!";
+			
+			var successBoxColor = "00c853"; // Background color for the success box
+			
+			var successBoxBorderStyle = "solid"; // Border Style  -  Example: solid, dotted, dashed, double
+			
+			var successBox_Border_Color = "00c853"; // Border color for success box
+			
+			var textColor = "fff"; // text color for success box
+			
+/* ================================  SUBSCRIBE FORM  =================================== */
+			
+	/* SET SUBSCRIBE FORM SUCCESS MESSAGE */
+	
+			var sub_submitMessage = "You have subscribed successfully.";
+			
+			var sub_successBoxColor = "00c853"; // Background color for the success box
+			
+			var sub_successBoxBorderStyle = "solid"; // Border Style  -  Example: solid, dotted, dashed, double
+			
+			var sub_successBox_Border_Color = "00c853"; // Border color for success box
+			
+			var sub_textColor = "fff"; // text color for success box
+			
+/* ================================  REPLACE GOOGLE FONT CODE HERE  =================================== */
+
+// Google Web Fonts
+
+  // Oswald
+  WebFontConfig = {
+    google: { families: [ 'Oswald:400,300,700:latin,latin-ext' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+  
+  
+  // Raleway
+  
+  WebFontConfig = {
+    google: { families: [ 'Raleway:400,100,200,300,500,600,700,800,900:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+
+
+
+
+
+
+
+
